59136decc2ad219a34fded7f596e6b375e958c03
Aditya Parghi <adityaparghi26@gmail.com>
2026-06-04 18:46:06 +0530

    Schema added

---
+++ Week-8-course-selling/db.js
@@ -0,0 +1,50 @@
+require('dotenv').config({
+    path: '../.env'
+});
+const dns  = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_COURSE);
+console.log("DB connected");
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+
+const userSchema = new Schema({
+    email: {type: String, unique: true},
+    password: String,
+    firstName: String,
+    lastName: String
+})
+
+const adminSchema = new Schema({
+    email: {type: String, unique: true},
+    password: String,
+    firstName: String,
+    lastName: String
+})
+
+const courseSchema =  new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
+})
+
+const purchaseSchema = new Schema({
+    userId: ObjectId,
+    courseId: ObjectId
+})  
+
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+module.exports = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
--- Week-8-course-selling/index.js
+++ Week-8-course-selling/index.js
@@ -1,16 +1,15 @@
 const express = require("express");
-const mongoose = require("mongoose");
+
 const {userRouter} = require("./routes/user");
 const {courseRouter} = require("./routes/course");
 const { adminRouter } = require("./routes/admin");
+const { mongoose } = require("mongoose");
+
 const app = express();
 app.use(express.json());
 
 app.use('/user', userRouter);
 app.use('/course', courseRouter);
 app.use('/admin', adminRouter);
-
-
-
 
 app.listen(3000);
