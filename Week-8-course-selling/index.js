c86e316aa32e23da71a6bceb321308dd65313797
Aditya Parghi <adityaparghi26@gmail.com>
2026-06-04 17:54:45 +0530

    skeleton code for routers

---
+++ Week-8-course-selling/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const {userRouter} = require("./routes/user");
+const {courseRouter} = require("./routes/course");
+const { adminRouter } = require("./routes/admin");
+const app = express();
+app.use(express.json());
+
+app.use('/user', userRouter);
+app.use('/course', courseRouter);
+app.use('/admin', adminRouter);
+
+
+
+
+app.listen(3000);
---
+++ Week-8-course-selling/routes/admin.js
@@ -0,0 +1,36 @@
+const {Router} = require("express");
+const adminRouter = Router();
+
+adminRouter.post('/signup', function(req, res){
+    res.json({
+        message: "admin signup"
+    })
+})
+
+adminRouter.post('/signin', function(req, res){
+    res.json({
+        message: "admin signin"
+    })
+})
+
+adminRouter.post('/course', function(req, res){
+    res.json({
+        message: "admin course"
+    })
+})
+
+adminRouter.put('/course', function(req, res){
+    res.json({
+        message: "admin course put"
+    })
+})
+
+adminRouter.get('/course/bulk', function(req, res){
+    res.json({
+        message: "admin all course"
+    })
+})
+
+module.exports = {
+    adminRouter: adminRouter
+}
