491eb29a7148a779507d18f1d7e6546b4309c84c
Aditya Parghi <adityaparghi26@gmail.com>
2026-09-02 16:24:01 +0530

    Tabs logic

--- week-9-react/react-learning/src/App.jsx
+++ week-9-react/react-learning/src/App.jsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { PostComponent } from "./Post";
 
 function App() {
-  const [posts, setPosts] = useState([]);
+  const[posts, setPosts] = useState([]);
+  const[currentTab, setCurrentTab] = useState(1);
+  const[loading, setLoading] = useState(false);
+  const[tabData, setTabData] = useState({});
 
   const postComponents = posts.map(post => <PostComponent
     name={post.name}
@@ -22,14 +25,45 @@ function App() {
     }])
   }
 
+  function removePost(){
+    setPosts(posts.slice(0, -1));
+  }
+
+  useEffect(function(){
+    setLoading(true);
+    fetch("https://jsonplaceholder.typicode.com/todos/"+currentTab).
+    then(async res => {
+      const json = await res.json();
+      setTabData(json);
+      setLoading(false);
+    })
+
+  },[currentTab])
+
   return (
     <div style={{background: "#dfe6e9", height: "100vh", }}>
-      <button onClick={addPost}>Add post</button>
+      <div style={{display:"flex", gap:"10px"}}>
+        <button onClick={addPost}>Add post</button>
+        <button onClick={removePost}>Remove Post</button>
+        <button>Tabs management {"-->"}</button>
+
+        <button onClick={() => setCurrentTab(1)}
+        style={{color: currentTab == 1 ? "red" : "black" }} >Todo1</button>
+
+        <button onClick={() => setCurrentTab(2)} 
+        style={{color: currentTab == 2 ? "red" : "black" }}>Todo2</button>
+        <button onClick={() => setCurrentTab(3)} 
+        style={{color: currentTab == 3 ? "red" : "black" }}>Todo3</button>
+        <button onClick={() => setCurrentTab(4)} 
+        style={{color: currentTab == 4 ? "red" : "black" }}>Todo4</button>
+      </div>
+
       <div style={{display: "flex", justifyContent: "center" }}>
         <div>
-          {postComponents}// Array of react elements
+          {postComponents}{/*Array of react elements*/}
         </div>
       </div>
+      {loading ? "Loading...." : tabData.title} 
     </div>
   )
 }
